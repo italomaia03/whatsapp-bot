e04a1b2c50f78ba321a95f76a1135143c05b235c
Ítalo F. O. Maia <109195782+italomaia03@users.noreply.github.com>
2025-03-03 21:13:02 +0000

    feat: map env variable

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,15 +1,17 @@
-type ENV_VARIABLES = {
-    PORT?: string,
-    DATABASE_URL?: string,
-    DATABASE_USER?: string,
-    DATABASE_PASSWORD?: string,
-    API_WHATSAPP_TOKEN?: string,
-}
+type EnvironmentVariables = {
+    PORT?: string;
+    DATABASE_URL?: string;
+    DATABASE_USER?: string;
+    DATABASE_PASSWORD?: string;
+    API_WHATSAPP_TOKEN?: string;
+    WHATSAPP_ACCESS_TOKEN?: string;
+};
 
-export const env: ENV_VARIABLES = {
-  PORT: process.env.PORT,
-  DATABASE_URL: process.env.DATABASE_URL,
-  DATABASE_USER: process.env.DATABASE_USER,
-  DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
-  API_WHATSAPP_TOKEN: process.env.API_WHATSAPP_TOKEN,
+export const env: EnvironmentVariables = {
+    PORT: process.env.PORT,
+    DATABASE_URL: process.env.DATABASE_URL,
+    DATABASE_USER: process.env.DATABASE_USER,
+    DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
+    API_WHATSAPP_TOKEN: process.env.API_WHATSAPP_TOKEN,
+    WHATSAPP_ACCESS_TOKEN: process.env.WHATSAPP_ACCESS_TOKEN,
 };
